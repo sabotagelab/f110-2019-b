fe0f6d8fea0ee0d3555dfc10c80d8764ddcf0b5a
Niraj Basnet <nirajbasnet2008@gmail.com>
2019-06-08 00:13:02 -0700

    added runtime analysis for pure pursuit

--- speed_daemons_pure_pursuit/scripts/create_waypoints.py
+++ speed_daemons_pure_pursuit/scripts/create_waypoints.py
@@ -10,30 +10,25 @@ class writeWaypoints:
         self.pastX = 0
         self.pastY = 0
         rospy.init_node('create_waypoints_node', anonymous=True)
+        rospy.Subscriber("/pf/viz/inferred_pose", PoseStamped, self.waypoint_recieved)
         self.file_path = rospy.get_param('~waypoint_filepath', '')
         if self.file_path == '':
             raise ValueError('No any file path for instruction file')
         open(self.file_path, 'w').close()
-        rospy.Subscriber("/pf/viz/inferred_pose", PoseStamped, self.waypoint_recieved)
 
     def waypoint_recieved(self, data):
         euclidDistance = math.sqrt(pow((data.pose.position.y - self.pastY),2) + pow((data.pose.position.x - self.pastX),2))
-    
-        if (euclidDistance > 0.05):
-            print(data.pose.position.x,data.pose.position.y)
+
+        if euclidDistance > 0.03:
             self.writeToCSV(data.pose.position.x, data.pose.position.y)
             self.pastX = data.pose.position.x
             self.pastY = data.pose.position.y
-           
-        
-
 
 
     def writeToCSV(self,x,y):
         with open(self.file_path, mode='a') as waypoint_file:
             waypoint_writer = csv.writer(waypoint_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             waypoint_writer.writerow([x, y, 0])
-            # waypoint_writer.writerow(['Erica Meyers', 'IT', 'March'])
 
 if __name__ == '__main__':
     waypoint_creator = writeWaypoints()
